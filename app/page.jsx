e1e48ee0a0832952412a28058e0c4621856bed5d
Dhaval <kdkothari7@gmail.com>
2024-06-11 20:53:44 +0530

    1st commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-red-400 bg-slate-700 min-h-screen flex justify-center items-center text-3xl">
+      <p>Github actions</p>
+    </div>
+  );
+}
