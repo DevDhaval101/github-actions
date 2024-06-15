fcb955e94e8bca58659660264ff6caa1c9e2c8d2
Dhaval Kothari <114064309+DevDhaval101@users.noreply.github.com>
2024-06-15 09:51:09 +0530

    edited

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-red-400 bg-slate-700 min-h-screen flex justify-center items-center text-3xl">
-      <p>Github actions</p>
+      <p>Github actions working?</p>
     </div>
   );
 }
